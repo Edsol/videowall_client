0639dc28edb00e421eb05db64e8a2116d94b64a9
Edoardo Soloperto <soloperto@nedgenerators.com>
2022-01-19 09:43:34 +0100

    urlList component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UrlList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-import * as axios from 'axios';
+import UrlList from './components/UrlList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UrlList
   },
-  created:function(){
-    axios.get('http://localhost:3000/frontend')
-    .then((response) =>{
-      console.log(response)
-    })
-  }
 }
 </script>
 
---
+++ src/components/UrlList.vue
@@ -0,0 +1,40 @@
+<template>
+  <table id="urlHistories" class='table'>
+        <thead>
+          <th>ID</th>
+          <th>date</th>
+          <th>url</th>
+          <th>port</th>
+        </thead>
+        <tbody>
+          <tr v-for="item in items" v-bind:key="item">
+            <td>{{item.id}}</td>
+            <td>{{item.datetime}}</td>
+            <td>{{item.url}}</td>
+            <td>{{item.display.port}}</td>
+          </tr>
+        </tbody>
+      </table>
+</template>
+
+<script>
+import * as axios from 'axios';
+
+export default {
+  data(){
+    return{
+      items: [],
+    }
+  },
+  created:function(){
+    axios.get('http://localhost:3000/frontend/urlHistorylist/10')
+    .then((response) =>{
+      this.items = response.data;
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
