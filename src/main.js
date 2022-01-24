c3bfa3467a52b43865a422bd4b25c1ed0beb22fd
Edoardo Soloperto <soloperto@nedgenerators.com>
2022-01-24 07:43:10 +0100

    axios backend request

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'vue-toast-notification/dist/theme-sugar.css';
 
-import ip from 'ip';
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faPhone, faTrashAlt, faTimesCircle, faRetweet } from "@fortawesome/free-solid-svg-icons";
@@ -22,14 +20,20 @@ const app = createApp(App)
 app.config.globalProperties.backendUrl = 'http://localhost:3000/frontend/';
 app.config.globalProperties.apiUrl = 'http://localhost:3000/api/';
 
-var ipAddress = ip.address();
-console.log('IP ADDRESS', ipAddress);
+axios.get('http://localhost:3000/api/getConfig')
+    .then((response) => {
+        console.log('axios main', response.data)
+    })
 
+
+
+// var ipAddress = ip.address();
+// console.log('IP ADDRESS', ipAddress);
 
 // if (process.env.NODE_ENV !== "development") {
 //     var ipAddress = ip.address();
-app.config.globalProperties.backendUrl = 'http://' + ipAddress + ':3000/frontend/';
-app.config.globalProperties.apiUrl = 'http://' + ipAddress + ':3000/api/';
+// app.config.globalProperties.backendUrl = 'http://' + ipAddress + ':3000/frontend/';
+// app.config.globalProperties.apiUrl = 'http://' + ipAddress + ':3000/api/';
 //     console.log('PRODUCTION', 'http://' + ip + ':3000/frontend/', 'http://' + ip + ':3000/api/')
 // }
 
