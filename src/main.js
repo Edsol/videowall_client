d79e6dcf297a3b982c47292ccc6dc8ab213d0e71
Edoardo Soloperto <soloperto@nedgenerators.com>
2022-01-24 07:31:24 +0100

    rebuild

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'vue-toast-notification/dist/theme-sugar.css';
 
+import ip from 'ip';
+
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faPhone, faTrashAlt, faTimesCircle, faRetweet } from "@fortawesome/free-solid-svg-icons";
@@ -19,6 +21,21 @@ const app = createApp(App)
 
 app.config.globalProperties.backendUrl = 'http://localhost:3000/frontend/';
 app.config.globalProperties.apiUrl = 'http://localhost:3000/api/';
+
+var ipAddress = ip.address();
+console.log('IP ADDRESS', ipAddress);
+
+
+// if (process.env.NODE_ENV !== "development") {
+//     var ipAddress = ip.address();
+app.config.globalProperties.backendUrl = 'http://' + ipAddress + ':3000/frontend/';
+app.config.globalProperties.apiUrl = 'http://' + ipAddress + ':3000/api/';
+//     console.log('PRODUCTION', 'http://' + ip + ':3000/frontend/', 'http://' + ip + ':3000/api/')
+// }
+
+
+
+
 app.config.globalProperties.emitter = emitter
 app.config.globalProperties.axios = axios
 
